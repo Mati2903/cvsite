ac5cb86fbd8af5c373ccd8b2162d3da65686aa9a
Mati2903 <mazurek321.dev@gmail.com>
2023-03-16 17:05:56 +0100

    change navbar code and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,74 @@
-import { navLinks } from "../constants/index";
 import Link from "next/link";
-import { useState } from "react";
-import { FaBars } from "react-icons/fa";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { navLinks } from "../constants/index";
 
 const Navbar = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const [activeLink, setActiveLink] = useState(null);
+	const router = useRouter();
 
-	const handleToggle = () => {
-		setIsOpen(!isOpen);
+	const [open, setOpen] = useState(false); //stan hamburgera
+
+	//funkcja do obsługi klika w menu hamburger zmieniająca stan (a przez to klasę) dla urządzeń mobilnych / dodaje też linię na dole navbara po jego otwarciu
+	const handleMenuClick = () => {
+		const navbar = document.querySelector(".nav");
+
+		if (window.innerWidth < 768) {
+			console.log(router.asPath);
+			!open ? setOpen(true) : setOpen(false);
+			!open
+				? navbar.classList.add("active")
+				: navbar.classList.remove("active");
+		}
 	};
 
-	const handleClick = (id) => {
-		setActiveLink(id);
-	};
+	//usuwanie klas w elementach po zrobieniu resize szerokości okna >= 768px - usuwa linię dzielącą menu i obraca logo do pozycji poziomej
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth >= 768) {
+				setOpen(false);
+
+				document.querySelector(".nav").classList.remove("active");
+			}
+		};
+		window.addEventListener("resize", handleResize);
+	});
+
+	//usuwanie klas open (zamykanie menu) po kliknięciu na stronie poza navbarem
+	useEffect(() => {
+		if (open) {
+			document.querySelector("main").addEventListener("click", () => {
+				setOpen(false);
+				document.querySelector(".nav").classList.remove("active");
+			});
+		}
+	}, [open]);
 
 	return (
-		<nav className="navbar">
-			<button className="navbar__toggle" onClick={handleToggle}>
-				<FaBars />
-			</button>
-			<ul className={`navbar__links ${isOpen ? "open" : ""}`}>
+		<nav className="nav">
+			<ul
+				onClick={handleMenuClick}
+				className={`nav-menu ${open ? "open" : ""}`}
+			>
+				{/* navLinks comes from constants - simulates db */}
 				{navLinks.map((link) => (
 					<li key={link.id}>
 						<Link
-							className={
-								activeLink === link.id ? "navbar__link active" : "navbar__link"
-							}
-							href={`#${link.id}`}
-							onClick={() => handleClick(link.id)}
+							href={link.href}
+							//check which element was clicked based on href value and add active class to clicked link
+							className={router.asPath == link.href ? "active" : ""}
 						>
 							{link.title}
 						</Link>
 					</li>
 				))}
 			</ul>
+			{/* hamburger */}
+			<div
+				className={`hamburger ${open ? "open" : ""}`}
+				onClick={handleMenuClick}
+			>
+				<div className="hamburger__icon"></div>
+			</div>
 		</nav>
 	);
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,22 +1,28 @@
+//============ navbar menu links ================
 export const navLinks = [
 	{
 		id: "home",
+		href: "/",
 		title: "Home",
 	},
 	{
 		id: "about",
+		href: "/#about",
 		title: "About",
 	},
 	{
 		id: "experience",
+		href: "/#experience",
 		title: "Experience",
 	},
 	{
 		id: "projects",
+		href: "/#projects",
 		title: "Projects",
 	},
 	{
 		id: "contact",
+		href: "/#contact",
 		title: "Contact",
 	},
 ];
