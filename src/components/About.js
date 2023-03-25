73328964f7e52904bcf8901f86a06d4da7d77b55
Mati2903 <mazurek321.dev@gmail.com>
2023-03-25 10:16:38 +0100

    fix vercel build error with prime symbols

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
-import {FaLaptopMedical, FaUniversity, FaLaptopCode} from "react-icons/fa";
-import {MdOutlineElectricBolt} from "react-icons/md";
-import {BiMicrochip} from "react-icons/bi";
+import { FaLaptopMedical, FaUniversity, FaLaptopCode } from "react-icons/fa";
+import { MdOutlineElectricBolt } from "react-icons/md";
+import { BiMicrochip } from "react-icons/bi";
 
 const About = () => {
 	return (
@@ -18,9 +18,11 @@ const About = () => {
 					My name is Mateusz and currently I work as{" "}
 					<a href="https://mmcm.pl/" target="__blank" className="work-link">
 						medical equipment service engineer
-					</a>&nbsp;<FaLaptopMedical />
-					. I do inspections and repairs of medical devices in hospitals. I also
-					repair other electronics devices, especially laptops.
+					</a>
+					&nbsp;
+					<FaLaptopMedical />. I do inspections and repairs of medical devices
+					in hospitals. I also repair other electronics devices, especially
+					laptops.
 				</p>
 			</motion.div>
 			<motion.div
@@ -38,8 +40,9 @@ const About = () => {
 						className="university-color"
 					>
 						Lublin University of Technology
-					</a>{" "}<FaUniversity/>{" "}
-					and obtained a master's degree in Electrical Engineering <MdOutlineElectricBolt/> at the{" "}
+					</a>{" "}
+					<FaUniversity /> and obtained a master&#8242;s degree in Electrical
+					Engineering <MdOutlineElectricBolt /> at the{" "}
 					<a
 						href="https://weii.pollub.pl/en/"
 						target="__blank"
@@ -48,9 +51,10 @@ const About = () => {
 						Faculty of Electrical Engineering and Computer Science
 					</a>
 					.<br />
-					During my studies I was learning microcontrollers programming <BiMicrochip/> . In my
-					engineering thesis I developed an on-board car computer which reads
-					engine work parameters from ECU and displays them on the screen.
+					During my studies I was learning microcontrollers programming{" "}
+					<BiMicrochip /> . In my engineering thesis I developed an on-board car
+					computer which reads engine work parameters from ECU and displays them
+					on the screen.
 				</p>
 			</motion.div>
 			<motion.div
@@ -61,7 +65,7 @@ const About = () => {
 				transition={{ delay: 0.3 }}
 			>
 				<p className="about__card-paragraph">
-					Since June 2021 I'm learning web development using
+					Since June 2021 I&#8242;m learning web development using
 					<span className="html-color"> HTML</span>,
 					<span className="css-color"> CSS</span>/
 					<span className="scss-color">SCSS</span>,
@@ -69,9 +73,9 @@ const About = () => {
 					<span className="react-color"> React</span>,
 					<span className="nextjs-color"> Next.js </span>and
 					<span className="nodejs-color"> Node.js</span>. <br />
-					Currently I'm looking for my first job as a frontend/fullstack
-					developer <FaLaptopCode/> to improve programming skills working on real-world
-					projects.
+					Currently I&#8242;m looking for my first job as a frontend/fullstack
+					developer <FaLaptopCode /> to improve programming skills working on
+					real-world projects.
 				</p>
 			</motion.div>
 		</section>
